ebf3e734e47510b2e61cb2a991e5e00b7e3a43ad
Dima <akmeev2011@yandex.ru>
2022-11-26 20:04:31 +0300

    Initial commit

---
+++ src/main/java/com/example/mockito_hw/MockitoHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockito_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mockito_hw/MockitoHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockito_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
